f52b1ed1f63ea23e0b9895b21ce082183167bf42
pancrasduan <dpqkindle@163.com>
2022-09-23 01:15:08 +0800

    add feature-decorator-pattern

---
+++ decorator-pattern/src/main/java/com/pancras/design/decorator/Main.java
@@ -0,0 +1,15 @@
+package com.pancras.design.decorator;
+
+/**
+ * @Author: Pancras
+ * @Date: 2022-09-22
+ * @Time: 22:10
+ * @Description:
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
